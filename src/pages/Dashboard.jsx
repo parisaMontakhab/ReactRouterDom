bd5c4ad3132e6f43d782ee9e83e3d0acb7ef3278
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-07 16:47:33 +0200

    feat: add Link cmpnnt in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard page</div>
+    <div>
+        <h1>Dashboard page</h1>
+      <Link to="/">see Home page</Link>
+    </div>
   )
 }
