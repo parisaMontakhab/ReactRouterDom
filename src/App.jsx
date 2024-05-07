f09504e5714dfa5babaefcad3111075172fb2b4b
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-07 16:28:26 +0200

    feat: call AppNav in App cmpnnt

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import "./App.css";
 import Home from "./pages/Home";
 import Posts from "./pages/Posts";
 import Dashboard from "./pages/Dashboard";
+import AppNav from "./components/AppNav";
 
 
 function App() {
   return (
 <div>
-
+<AppNav/>
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/posts" element={<Posts/>}/>
