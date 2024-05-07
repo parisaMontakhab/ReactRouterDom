71ca093f8a5cb361f6c98c7f668e2667a5c9a468
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-07 16:41:48 +0200

    refactor: replace the a tag to Link component and add to atribiut

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div>
+        <h1>Home Page</h1>
+        <Link to='/posts'> see latest posts</Link>
+    </div>
   )
 }
