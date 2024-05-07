7632d7dced6fddbefffb970958558f659277a1d2
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-07 16:44:30 +0200

    refactor: add Link componnt in Post cmpnnt

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Posts() {
   return (
-    <div>Post page</div>
+    <div>
+        <h1>Posts page</h1>
+        <Link to="/dashboard">see Dashboard page</Link>
+    </div>
   )
 }
