9c8b6d3c7f0b09ae43b7f20281e5bff5075b0478
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-08 16:19:51 +0200

    feat: add  Layout as element in Route and Outlet in AppNav cmpnnt

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import Home from "./pages/Home";
 import Posts from "./pages/Posts";
 import Dashboard from "./pages/Dashboard";
 import AppNav from "./components/AppNav";
+import Layout from "./components/Layout";
 
 
 function App() {
   return (
 <div>
-<AppNav/>
+  
+
   <Routes>
+    <Route element={<Layout/>}>
+
     <Route path="/" element={<Home/>}/>
     <Route path="/posts" element={<Posts/>}/>
     <Route path="/dashboard" element={<Dashboard/>}/>
+
+    </Route>
+    
   </Routes>
  
 </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AppNav from './AppNav'
+import { Outlet } from 'react-router-dom'
 
 export default function () {
   return (
     <div>
         <AppNav/>
+        <Outlet/>
         <footer style={{marginTop:150}}> This is footer</footer>
     </div>
   )
