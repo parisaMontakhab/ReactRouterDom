e8336cd01ac3f7dad76f1d198fb6d38c469666bd
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-08 16:13:22 +0200

    feat: add AppNav compnnt and footer in Layout componnt

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AppNav from './AppNav'
+
+export default function () {
+  return (
+    <div>
+        <AppNav/>
+        <footer style={{marginTop:150}}> This is footer</footer>
+    </div>
+  )
+}
